6cc7511f63a804b40420c6283ad6e9f6df355f8b
Lőrinc Kóródi <korodilorinc@gmail.com>
2025-06-05 22:25:51 +0200

    Moved genetic events into Genome class

--- src/fungus.js
+++ src/fungus.js
@@ -3,6 +3,7 @@ import { idGenerator, sample, wrap } from "./util.js";
 import { sim } from "./main.js";
 import { Node, Tree } from "./tree.js";
 import { Genome } from "./genome.js";
+import { Gene } from "./genome.js";
 
 const genID = idGenerator();
 
@@ -34,6 +35,7 @@ export default class Fungus {
     }
 
     vegetative() {
+        
         return true;
     }
 
@@ -104,71 +106,6 @@ export default class Fungus {
 
         //console.log(spore.genome)
         return spore;
-    }
-
-    cutNpaste(chr1, chr2) {
-        Object.keys(chr1).forEach((gene) => {
-            if (sim.rng.random() < 0.01) {
-                chr2[gene] = chr1[gene];
-                delete chr1[gene];
-            }
-        });
-    }
-
-    geneLoss(chr) {
-        Object.keys(chr).forEach((gene) => {
-            if (sim.rng.random() < sim.config.loss_rate) {
-                delete chr[gene];
-            }
-        });
-    }
-
-    geneGain(chr) {
-        const genes = {
-            a: "hc",
-            b: "hc",
-            c: "hc",
-            x: "toxin",
-            y: "toxin",
-            z: "toxin",
-            k: "junk",
-            l: "junk",
-            m: "junk",
-        };
-
-        Object.keys(genes).forEach((gene) => {
-            if (sim.rng.random() < sim.config.gain_rate) {
-                chr[gene] = genes[gene];
-            }
-        });
-    }
-
-    hgt(network) {
-        if (Object.keys(this.genome.mobile).length == 0) return;
-
-        Object.keys(network.genome.mobile).forEach((gene) => {
-            if (!gene in this.genome.mobile) {
-                this.genome.mobile[gene] = network.genome.mobile[gene];
-            }
-        });
-
-        let colour = ["", "", ""];
-
-        Object.keys(this.genome.core).forEach((gene) => {
-            if (gene == "x") colour[0] = "x";
-            if (gene == "y") colour[1] = "y";
-            if (gene == "z") colour[2] = "z";
-        });
-
-        Object.keys(this.genome.mobile).forEach((gene) => {
-            if (gene == "x") colour[0] = "x";
-            if (gene == "y") colour[1] = "y";
-            if (gene == "z") colour[2] = "z";
-        });
-
-        colour = colour.join("");
-
-        this.colour = colour == "" ? "none" : colour;
     }
 }
 
--- src/genome.js
+++ src/genome.js
@@ -1,23 +1,99 @@
-export class Genome{
-    constructor(coreGenes, accGenes = undefined){
-        if(!Array.isArray(coreGenes)){
-            console.error(`coreGenes: ${coreGenes} is not an Array!`);
+import { sim } from "./main.js";
 
-        }else{
+export class Genome {
+    constructor(coreGenes, accGenes = undefined) {
+        if (!Array.isArray(coreGenes)) {
+            console.error(`coreGenes: ${coreGenes} is not an Array!`);
+        } else {
             this.core = coreGenes;
         }
-        
-        if(!Array.isArray(accGenes) && accGenes != undefined){
+
+        if (!Array.isArray(accGenes) && accGenes != undefined) {
             console.error(`accGenes: ${accGenes} is not an Array!`);
-        }else{
+        } else {
             this.acc = accGenes;
         }
-        
     }
+
+    static cutNPaste(chr1, chr2) {
+        let newChr1 = [...chr1];
+        let newChr2 = [...chr2];
+
+        for (let i = 0; i < chr1.length; i++) {
+            if (sim.rng.random() < 0.01) {
+                newChr2.splice(i, 0, chr1[i]);
+                newChr1.splice(i, 1);
+            }
+        }
+
+        for (let i = 0; i < chr2.length; i++) {
+            if (sim.rng.random() < 0.01) {
+                newChr1.splice(i, 0, chr2[i]);
+                newChr2.splice(i, 1);
+            }
+        }
+
+        return [newChr1, newChr2];
+    }
+
+    static geneLoss(chr) {
+        let newChr = [...chr];
+
+        for (let i = 0; i < chr.length; i++) {
+            if (sim.rng.random() < 0.01) {
+                newChr.splice(i, 1);
+            }
+        }
+
+        return newChr;
+    }
+
+    static geneGain(chr) {
+        let newChr = [...chr];
+
+        for (let gene of Object.keys(Genome.genes)) {
+            if (sim.rng.random() < 0.01) {
+                newChr.push(new Gene(gene, Genome.genes[gene]));
+            }
+        }
+
+        return newChr;
+    }
+
+    static horizontalTransfer(genome1, genome2) {
+        if (!genome1.acc || !genome2.acc) return [genome1, genome2];
+
+        let newGenome1 = new Genome(genome1.core, genome1.acc);
+        let newGenome2 = new Genome(genome2.core, genome2.acc);
+
+        if (newGenome1.acc.length > 0 && newGenome2.acc.length == 0) {
+            newGenome1.acc.forEach(gene =>{
+                newGenome2.acc.push(new Gene(gene.name, gene.type))
+            })
+        } else if (newGenome1.acc.length == 0 && newGenome2.acc.length > 0) {
+            newGenome2.acc.forEach(gene =>{
+                newGenome1.acc.push(new Gene(gene.name, gene.type))
+            })
+        }
+
+        return [newGenome1, newGenome2];
+    }
+
+    static genes = {
+        a: "house_keeping",
+        b: "house_keeping",
+        c: "house_keeping",
+        x: "pathogenicity",
+        y: "pathogenicity",
+        z: "pathogenicity",
+        k: "junk",
+        l: "junk",
+        m: "junk",
+    };
 }
 
-export class Gene{
-    constructor(name, type, ){
+export class Gene {
+    constructor(name, type) {
         this.name = name;
         this.type = type;
     }
